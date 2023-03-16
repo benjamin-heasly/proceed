5896b82cb1d42082dd8a9ff457d049405cdb4304
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-03-16 12:08:12 -0400

    Fix sphinx css formatting and config.

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,12 @@
 # General project config, like project name and version, should stay in pyproject.toml.
 # We can pass general project values to sphinx-build using the -D command line option.
 
+# Unclear why "_templates" is relative to the docs/source/ dir,
+# while "source/_static" is relative to the docs/ dir itself.
+# But this is what seems to work -- so whatever!
 templates_path=["_templates"]
-html_static_path=["_static"]
-
+html_static_path=["source/_static"]
+html_css_files=["custom.css"]
 html_theme="pyramid"
 
 extensions=[
